37a96aacd991df2b38f5694720b1fec870cad3d9
george <7jagjag@gmail.com>
2022-10-26 18:03:31 +0200

    add router

--- frontend/src/components/Root.tsx
+++ frontend/src/components/Root.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@mui/material/styles'
+import { RouterProvider } from 'react-router-dom'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
@@ -7,11 +8,14 @@ import '@fontsource/roboto/700.css'
 import 'config/index.css'
 import theme from 'config/theme'
 import Layout from './Layout'
+import router from 'config/router'
 
 const Root = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>Content</Layout>
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
     </ThemeProvider>
   )
 }
